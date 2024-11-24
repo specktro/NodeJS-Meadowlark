36ad536536da2ac90b46629b1edc8fb4dd5fd70d
@specktro <specktro@nonull.mx>
2024-11-23 22:56:49 -0600

    feat: Add get routes for / and /about with get method

--- meadowlark.js
+++ meadowlark.js
@@ -2,15 +2,25 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+    res.type('text/plain')
+    res.send('Meadowlark Travel')
+})
+
+app.get('/about', (req, res) => {
+    res.type('text/plain');
+    res.send('About Meadowlark Travel')
+})
+
 // Custom 404 page
 app.use((req, res) => {
-    res.type('text/plain');
+    res.type('text/plain')
     res.status(404)
     res.send('404 - Not Found')
 })
 
 // Custom 500 page
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
     console.error(err.message)
     res.type('text/plain')
     res.status(500)
@@ -18,6 +28,6 @@ app.use((err, req, res, next) => {
 })
 
 app.listen(port, () => console.log(
-    `Express started on http://localhost:${port};` +
+    `Express started on http://localhost:${port}; ` +
     `press Ctrl-C to terminate.`
 ))
