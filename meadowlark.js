1720baa22da41a078ec0cac1d83a6efc83082af2
@specktro <specktro@nonull.mx>
2024-11-24 23:01:22 -0600

    refactor: Create a handlers lib js file to make project modular

--- meadowlark.js
+++ meadowlark.js
@@ -3,33 +3,18 @@ const expressHandlebars= require('express-handlebars')
 const app = express()
 const port = process.env.PORT || 3000
 
-const fortune = require('./lib/fortune')
+const handlers = require('./lib/handlers')
 
-app.engine('handlebars', expressHandlebars.engine({
-    defaultLayout: 'main',
-}))
+app.engine('handlebars', expressHandlebars.engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 app.use(express.static(__dirname + '/public'))
-
-app.get('/', (req, res) => res.render('home'))
-
-app.get('/about', (req, res) => {
-    res.render('about', { fortune: fortune.getFortune() })
-})
-
+app.get('/', handlers.home)
+app.get('/about', handlers.about)
 // Custom 404 page
-app.use((req, res) => {
-    res.status(404)
-    res.render('404')
-})
-
+app.use(handlers.notFound)
 // Custom 500 page
-app.use((err, req, res) => {
-    console.error(err.message)
-    res.status(500)
-    res.render('500')
-})
+app.use(handlers.serverError)
 
 app.listen(port, () => console.log(
     `Express started on http://localhost:${port}; ` +
