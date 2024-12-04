3370209347dc63412a07014b4bf08d8579fe7f1a
@specktro <specktro@nonull.mx>
2024-12-04 07:52:50 -0600

    feat: Add form with express framework

--- meadowlark.js
+++ meadowlark.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const expressHandlebars= require('express-handlebars')
+const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 3000
 
@@ -7,10 +8,14 @@ const handlers = require('./lib/handlers')
 
 app.engine('handlebars', expressHandlebars.engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(express.static(__dirname + '/public'))
 app.get('/', handlers.home)
 app.get('/about', handlers.about)
+app.get('/newsletter-signup', handlers.newsletterSignup)
+app.post('/newsletter-signup/process', handlers.newsletterSignupProcess)
+app.get('/newsletter-signup/thank-you', handlers.newsletterSignupThankYou)
 // Custom 404 page
 app.use(handlers.notFound)
 // Custom 500 page
